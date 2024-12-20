16c14013acffb744e39ae537ad35a48a01df3dae
Hashtagich <antonchikz@mail.ru>
2024-12-20 13:17:47 +0300

    ThemePageORM. Сделал модель и добавил в админку Модель тема главной страницы.

--- backend/src/admin.py
+++ backend/src/admin.py
@@ -2,6 +2,7 @@ from sqladmin import Admin
 from sqladmin import ModelView
 
 from src.app.models.book.book import BookORM, FotoBookORM, AuthorBookORM, FeedbackORM
+from src.app.models.theme_page.theme_page import ThemePageORM
 
 
 class BaseModelView(ModelView):
@@ -72,7 +73,6 @@ class FotoBookAdmin(BaseModelView, model=FotoBookORM):
     name_plural = "Фото книг"
 
     BASE_LIST = [BaseModelView.BASE_LIST[0]] + [
-        FotoBookORM.id,
         FotoBookORM.foto,
         FotoBookORM.book_id,
         FotoBookORM.book
@@ -96,7 +96,6 @@ class AutorBookAdmin(BaseModelView, model=AuthorBookORM):
     name_plural = "Авторы книг"
 
     BASE_LIST = [BaseModelView.BASE_LIST[0]] + [
-        AuthorBookORM.id,
         AuthorBookORM.title,
         AuthorBookORM.foto,
         AuthorBookORM.book_id,
@@ -126,7 +125,6 @@ class FeedbackAdmin(BaseModelView, model=FeedbackORM):
     name_plural = "Отзывы"
 
     BASE_LIST = [BaseModelView.BASE_LIST[0]] + [
-        FeedbackORM.id,
         FeedbackORM.is_active,
         FeedbackORM.author,
         FeedbackORM.text
@@ -150,10 +148,50 @@ class FeedbackAdmin(BaseModelView, model=FeedbackORM):
     }
 
 
+class ThemePageAdmin(BaseModelView, model=ThemePageORM):
+    name = "Тема главной страницы"
+    name_plural = "Темы главной страницы"
+
+    BASE_LIST = [BaseModelView.BASE_LIST[0]] + [
+        ThemePageORM.is_active,
+        ThemePageORM.title,
+    ] + BaseModelView.BASE_LIST[1:]
+
+    column_searchable_list = [
+        ThemePageORM.title,
+    ]
+
+    column_sortable_list = BASE_LIST
+    column_list = BASE_LIST
+
+    column_labels = {
+        ThemePageORM.id: "ID",
+        ThemePageORM.title: "Название темы",
+        ThemePageORM.header: "Фото шапки",
+        ThemePageORM.foto_1: "Фото №1",
+        ThemePageORM.foto_2: "Фото №2",
+        ThemePageORM.foto_3: "Фото №3",
+        ThemePageORM.foto_4: "Фото №4",
+        ThemePageORM.foto_5: "Фото №5",
+        ThemePageORM.foto_6: "Фото №6",
+        ThemePageORM.foto_7: "Фото №7",
+        ThemePageORM.foto_8: "Фото №8",
+        ThemePageORM.foto_9: "Фото №9",
+        ThemePageORM.foto_10: "Фото №10",
+        ThemePageORM.foto_11: "Фото №11",
+        ThemePageORM.footer_bg: "Фото футера",
+        ThemePageORM.footer_logo: "Логотип футера",
+        ThemePageORM.is_active: "Действующая",
+        ThemePageORM.creation_date: "Дата создания",
+        ThemePageORM.update_date: "Дата обновления"
+    }
+
+
 def create_admin(app, engine):
     admin = Admin(app, engine, title="ACT")
     admin.add_view(BookAdmin)
     admin.add_view(FotoBookAdmin)
     admin.add_view(AutorBookAdmin)
     admin.add_view(FeedbackAdmin)
+    admin.add_view(ThemePageAdmin)
     return admin
